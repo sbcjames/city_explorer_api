bb2ab1dbf543d657d4b3611e2bd37ed877fb8516
James Swift <sbcjames86@yahoo.com>
2020-09-19 12:39:19 -0700

    corrected schema for new spelling

--- schema.sql
+++ schema.sql
@@ -11,8 +11,7 @@ CREATE TABLE locations(
 
 CREATE TABLE weather(
     id SERIAL PRIMARY KEY,
-    search_query VARCHAR(255),
-    forecast VARCHAR(255),
-    time VARCHAR(255),
+    formatted_query VARCHAR(255),
+    forecast TEXT,
     date_entered VARCHAR(255)
 );
