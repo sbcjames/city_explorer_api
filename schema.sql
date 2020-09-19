d67d3f5bb01d16165368eb321bb9666cc5da70e1
James Swift <sbcjames86@yahoo.com>
2020-09-19 11:10:47 -0700

    update to table format

--- schema.sql
+++ schema.sql
@@ -1,16 +1,18 @@
-DROP TABLE IF EXISTS locations, weather;
+DROP TABLE IF EXISTS locations;
+DROP TABLE IF EXISTS weather;
 
-CREATE TABLE locations (
+CREATE TABLE locations(
   id SERIAL PRIMARY KEY,
   search_query VARCHAR(255),
   formatted_query VARCHAR(255),
-  latitude DECIMAL,
-  longitude DECIMAL
+  latitude decimal,
+  longitude decimal
 );
 
-CREATE TABLE weather (
-  id SERIAL PRIMARY KEY,
-  search_query VARCHAR(255),
-  forecast VARCHAR(255),
-  time_stamp VARCHAR(255)
+CREATE TABLE weather(
+    id SERIAL PRIMARY KEY,
+    search_query VARCHAR(255),
+    forecast VARCHAR(255),
+    time VARCHAR(255),
+    date_entered VARCHAR(255)
 );
